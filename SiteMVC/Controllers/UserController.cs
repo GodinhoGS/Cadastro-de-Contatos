ccb56f79b65660314fd47fbf00b34516173c30d6
Gustavo Godinho <gustavogodinhosoares@gmail.com>
2023-07-27 13:11:41 -0300

    user control

--- SiteMVC/Controllers/UserController.cs
+++ SiteMVC/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SiteMVC.Models;
 using SiteMVC.Repository;
+using System;
 using System.Collections.Generic;
 
 namespace SiteMVC.Controllers
@@ -17,6 +18,33 @@ namespace SiteMVC.Controllers
         {
             List<UserModel> user = _userRepository.GetAll();
             return View(user);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserModel user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _userRepository.Add(user);
+                    TempData["SuccessMessage"] = "User successfully created";
+                    return RedirectToAction("Index");
+                }
+
+                return View(user);
+            }
+            catch (Exception error)
+            {
+                TempData["ErrorMessage"] = $"Oops, we were unable to register your user, try again, error detail: {error.Message}";
+                return RedirectToAction("Index");
+                throw;
+            }
         }
     }
 }
--- SiteMVC/Models/UserModel.cs
+++ SiteMVC/Models/UserModel.cs
@@ -1,22 +1,22 @@
 ﻿using SiteMVC.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SiteMVC.Models
 {
     public class UserModel
     {
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "Type the user Name")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "Type the user Login!")]
         public string Login { get; set; }
-
+        [Required(ErrorMessage = "Type the user E-mail")]
+        [EmailAddress(ErrorMessage = "This E-mail is not valid!")]
         public string Email { get; set; }
-
         public ProfileEnum Profile { get; set; }
-
+        [Required(ErrorMessage = "Type the user Password!")]
         public string Password { get; set; }
-
         public DateTime RegisterDate { get; set; }
         public DateTime? UpdateDate { get; set; }
     }
